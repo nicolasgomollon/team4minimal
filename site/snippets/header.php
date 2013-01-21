01a284a3fa4063938582ab7b63935765c722b6f6
Nicolas Gomollon <nicolasgomollon@techno-magic.com>
2013-01-20 16:09:19 -0800

    Adds favicon.ico for Internet Explorer.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -6,13 +6,12 @@
 	<meta name="description" content="<?php echo html($site->description()) ?>" />
 	<meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
 	<meta name="robots" content="index, follow" />
-	<link rel="icon" type="image/png" href="/favicon.png">
-<!-- 	<?php echo css('assets/stylesheets/styles.css') ?> -->
+	<link rel="icon" type="image/png" href="favicon.png" />
 	<link rel="stylesheet" type="text/css" href="assets/stylesheets/style.css" />
 	<link rel="stylesheet" type="text/css" href="assets/stylesheets/style.mobile.css" media="all and (max-device-width: 480px)" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
 	<!--[if IE]>
-		<link rel="shortcut icon" href="/favicon.ico" />
+		<link rel="shortcut icon" href="favicon.ico" />
 		<link rel="stylesheet" type="text/css" href="assets/stylesheets/style.ie.css" />
 		<script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
