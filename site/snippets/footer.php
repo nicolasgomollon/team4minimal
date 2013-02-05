2335ad55b0f8ea78751cfc9fec34c1d7b53be8d2
Nicolas Gomollon <nicolasgomollon@techno-magic.com>
2013-02-04 16:26:32 -0800

    Removes Connect page. Adds social icons to footer. Removes PIE.htc dependency.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,3 +1,12 @@
+	<social>
+		<div class="center">
+			<a href="http://www.twitter.com/team4element"><div id="socialIconTwitter"></div></a>
+			<a href="http://www.facebook.com/team4element"><div id="socialIconFacebook"></div></a>
+			<a href="http://www.youtube.com/team4element"><div id="socialIconYouTube"></div></a>
+			<a href="http://www.instagram.com/team4element"><div id="socialIconInstagram"></div></a>
+			<a href="http://www.github.com/team4element"><div id="socialIconGitHub"></div></a>
+		</div>
+	</social>
 	<footer>
 		<div><?php echo $site->copyright() ?></div>
 	</footer>
