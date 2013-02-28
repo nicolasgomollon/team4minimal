9407fdce17c0719bf08caee4db9e4e3ae4b6161f
Nicolas Gomollon <nicolasgomollon@yahoo.com>
2013-02-27 16:42:34 -0800

    Adds sysadmin account to Kirby CMS Panel.

--- site/panel/accounts/sysadmin.php
+++ site/panel/accounts/sysadmin.php
@@ -5,6 +5,7 @@
 # for your passwords. read more about encryption in the
 # docs: http://getkirby.com/docs/panel/accounts
 
-username: admin
-password: adminpassword
+username: sysadmin
+password: 5d647b6bbaba9d98273446c6d9172a3f
+encrypt:  md5
 language: en
