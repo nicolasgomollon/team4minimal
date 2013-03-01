2b5393437850d3f5491d11d2de08341e19b7ca29
Nicolas Gomollon <nicolasgomollon@yahoo.com>
2013-03-01 13:47:26 -0800

    Adds blogger account to Kirby CMS Panel.

---
+++ site/panel/accounts/blogger.php
@@ -0,0 +1,11 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# setup your account credentials here
+# it's highly recommended to use md5 or sha1 encryption 
+# for your passwords. read more about encryption in the
+# docs: http://getkirby.com/docs/panel/accounts
+
+username: blogger
+password: d1cd3fb6574d60e3e8b8e748b4e17f79
+encrypt:  md5
+language: en
